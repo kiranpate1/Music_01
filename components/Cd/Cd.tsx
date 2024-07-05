c52185d5897eaf9b8d344f01a09eedc4612691ee
kiranpate1 <kp8568@gmail.com>
2024-07-04 23:49:48 -0400

    More animation tweaks

--- components/Cd/Cd.tsx
+++ components/Cd/Cd.tsx
@@ -4,6 +4,7 @@ import React, {
   useState,
   useEffect,
   useLayoutEffect,
+  use,
 } from "react";
 import {
   motion,
@@ -11,6 +12,7 @@ import {
   animate,
   useSpring,
   useTransform,
+  useMotionValue,
 } from "framer-motion";
 
 type props = {
@@ -47,45 +49,28 @@ const Cd = ({ size }: props) => {
     setWindowSize({ width, height });
   };
 
-  const fuckthisshit = useTransform(
+  const rotateX = useTransform(
     motionValue(cursorPosition.y),
     (latest: number) =>
       ((latest - windowSize.height / 2) / (windowSize.height / 2)) * -25
   );
 
-  const rotateX = useSpring(
-    useTransform(
-      motionValue(cursorPosition.y),
-      (latest: number) =>
-        ((latest - windowSize.height / 2) / (windowSize.height / 2)) * -25
-    ),
-    {
-      stiffness: 450,
-      damping: 100,
-    }
-  );
-
-  const fuckmylife = useTransform(
+  const rotateY = useTransform(
     motionValue(cursorPosition.x),
     (latest: number) =>
       ((latest - windowSize.width / 2) / (windowSize.width / 2)) * 25
   );
 
-  const jdjhswdbw = useSpring(fuckmylife, {
-    stiffness: 450,
-    damping: 100,
-  });
+  const translateX = useTransform(
+    motionValue(cursorPosition.y),
+    (latest: number) =>
+      -(latest - windowSize.height / 2) / (windowSize.height / 2)
+  );
 
-  const rotateY = useSpring(
-    useTransform(
-      motionValue(cursorPosition.x),
-      (latest: number) =>
-        ((latest - windowSize.width / 2) / (windowSize.width / 2)) * 25
-    ),
-    {
-      stiffness: 450,
-      damping: 100,
-    }
+  const translateY = useTransform(
+    motionValue(cursorPosition.x),
+    (latest: number) =>
+      -(latest - windowSize.width / 2) / (windowSize.width / 2)
   );
 
   return (
@@ -101,7 +86,7 @@ const Cd = ({ size }: props) => {
         }}
       >
         <motion.div
-          className="relative flex justify-center items-center"
+          className="relative flex justify-center items-center transition-transform duration-[900ms] ease-out"
           style={{
             width: size,
             height: size,
@@ -110,11 +95,16 @@ const Cd = ({ size }: props) => {
             transformStyle: "preserve-3d",
             rotateX: rotateX,
             rotateY: rotateY,
-            // rotateY: rotateY,
             // backgroundImage: `conic-gradient(#000 0%, #919191 10%, #000 20%, #000 50%, #919191 60%, #000 70%, #000 100%)`,
           }}
         >
-          <div className="absolute w-[97.5%] h-[97.5%] z-[2] mix-blend-hard-light">
+          <div className="absolute w-[97.5%] h-[97.5%] flex justify-center items-center z-[2] mix-blend-hard-light">
+            <img
+              className="absolute w-full h-full z-[4] mix-blend-plus-darker"
+              src="/images/sat2.png"
+              alt="sat2"
+              style={{ objectFit: "cover" }}
+            />
             <img
               className="absolute w-full h-full z-[3] mix-blend-soft-light"
               src="/images/sat.png"
@@ -128,8 +118,8 @@ const Cd = ({ size }: props) => {
               style={{ objectFit: "cover" }}
             />
             <motion.img
-              className="absolute w-full h-full z-[1]"
-              src="/images/bey-3.png"
+              className="absolute w-[99.5%] h-[99.5%] z-[1]"
+              src="/images/bey-1.png"
               alt="bey"
               style={{ objectFit: "cover" }}
               animate={{ rotate: -360 }}
@@ -140,12 +130,20 @@ const Cd = ({ size }: props) => {
               }}
             />
           </div>
-          <img
-            className="absolute w-full h-full z-[1]"
-            src="/images/cd.png"
-            alt="cd"
-            style={{ objectFit: "cover" }}
-          />
+          <div className="absolute w-full h-full z-[1]">
+            <img
+              className="absolute w-full h-full z-[1]"
+              src="/images/cd1.png"
+              alt="cd"
+              style={{ objectFit: "cover" }}
+            />
+            <motion.img
+              className="absolute w-[99.7%] h-[99.7%] z-[2]"
+              src="/images/cd2.png"
+              alt="cd"
+              style={{ objectFit: "cover", x: translateX, y: translateY }}
+            />
+          </div>
         </motion.div>
       </div>
     </div>
