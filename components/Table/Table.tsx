227f5abb5c10b3b89b7b2f2d423aa5ea3f44f698
kiranpate1 <kp8568@gmail.com>
2024-07-11 16:17:25 -0400

    updates

--- components/Table/Table.tsx
+++ components/Table/Table.tsx
@@ -1,5 +1,11 @@
 import React, { MutableRefObject, useRef, useState, useEffect } from "react";
-import { motion } from "framer-motion";
+import {
+  motion,
+  useTransform,
+  motionValue,
+  useSpring,
+  useMotionValue,
+} from "framer-motion";
 import { e } from "mathjs";
 import "./style.css";
 
@@ -9,7 +15,28 @@ type Props = {
 
 const Table = ({ height }: Props) => {
   const toneArmRef = useRef() as MutableRefObject<HTMLDivElement | null>;
-  const [toneArmDegrees, setToneArmDegrees] = useState(0);
+  const [toneArmDegrees, setToneArmDegrees] = useState(-17);
+  const [isOn, setIsOn] = useState(false);
+  const [spin, setSpin] = useState(0);
+
+  useEffect(() => {
+    let intervalId: NodeJS.Timeout | undefined = undefined;
+    if (isOn) {
+      // for (let i = 0; i < 100; i++) {
+      //   intervalId = setInterval(() => {
+      //     setSpin((prevSpin) => (prevSpin + 0.01 * i) % 361);
+      //   }, 10);
+      // }
+      intervalId = setInterval(() => {
+        setSpin((prevSpin) => (prevSpin + 1) % 361);
+      }, 8);
+    } else {
+      clearInterval(intervalId);
+    }
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [isOn]);
 
   function moveToneArm(event: MouseEvent | TouchEvent | PointerEvent) {
     if (!toneArmRef.current) return;
@@ -21,6 +48,12 @@ const Table = ({ height }: Props) => {
     const degrees = (angle * 180) / Math.PI - 120;
 
     setToneArmDegrees(degrees);
+
+    if (degrees > -13) {
+      setIsOn(true);
+    } else {
+      setIsOn(false);
+    }
   }
 
   return (
@@ -56,15 +89,16 @@ const Table = ({ height }: Props) => {
               alt="RINGS"
             />
             <motion.img
-              className="absolute w-full h-full z-[4]"
+              className="absolute w-full h-full z-[4] spin"
               src="/images/SPIN.png"
               alt="SPIN"
-              animate={{ rotate: 360 }}
-              transition={{
-                repeat: Infinity,
-                duration: 3,
-                ease: "linear",
-              }}
+              style={{ rotate: spin }}
+              // animate={{ rotate: 360 }}
+              // transition={{
+              //   repeat: Infinity,
+              //   duration: 3,
+              //   ease: "linear",
+              // }}
             />
           </div>
         </div>
@@ -97,23 +131,55 @@ const Table = ({ height }: Props) => {
             />
           </motion.div>
         </div>
-        {/* <div className="absolute left-[5.75%] bottom-[7.1%] z-[3] flex justify-start items-end mix-blend-plus-lighter">
+        <div className="absolute left-[5.75%] bottom-[7.1%] z-[3] flex justify-start items-end mix-blend-plus-lighter">
           <img
-            className="absolute translate-x-[-20%] translate-y-[20%]"
-            style={{ minWidth: height * (200 / height) + "px" }}
+            className="absolute translate-x-[-20%] translate-y-[20%] transition-all duration-300"
+            style={{
+              minWidth: height * (200 / height) + "px",
+              opacity: isOn ? 1 : 0,
+            }}
             src="/images/LIGHT.png"
             alt="light"
           />
-        </div> */}
+        </div>
         <div className="absolute left-[5.75%] bottom-[7.1%] z-[3] flex justify-start items-end">
           <img
-            className="absolute cursor-pointer rounded-[50%]"
+            className="absolute cursor-pointer rounded-[50%] duration-200 button"
             style={{
               minWidth: height * (50 / height) + "px",
-              boxShadow: "8px 8px 16px 0 rgba(0, 0, 0, 0.5)",
+              boxShadow: "8px 8px 8px 0 rgba(0, 0, 0, 0.25)",
+              scale: 1,
             }}
             src="/images/BUTTON.png"
             alt="button"
+            onMouseDown={() => {
+              const buttonElement = document.querySelector(
+                ".button"
+              ) as HTMLImageElement;
+              if (buttonElement) {
+                buttonElement.style.boxShadow =
+                  "0px 0px 2px 0 rgba(0, 0, 0, 0.25)";
+                buttonElement.style.transform = "scale(0.95)";
+              }
+            }}
+            onClick={() => {
+              // const springValue = useSpring(useMotionValue(0), {
+              //   stiffness: 1500,
+              //   damping: 100,
+              // });
+              setToneArmDegrees(isOn ? -17 : 0);
+              setIsOn((prevIsOn) => !prevIsOn);
+            }}
+            onMouseUp={() => {
+              const buttonElement = document.querySelector(
+                ".button"
+              ) as HTMLImageElement;
+              if (buttonElement) {
+                buttonElement.style.boxShadow =
+                  "8px 8px 8px 0 rgba(0, 0, 0, 0.25)";
+                buttonElement.style.transform = "scale(1)";
+              }
+            }}
           />
         </div>
       </div>
